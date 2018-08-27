b7734bb80a5aeaa33555b70119bee536f37d068a
AliAkbar shahi <AliAkbar.Shahi@MahanAir.Aero>
2018-08-27 17:27:42 +0430

    add percent function

--- js/calc.js
+++ js/calc.js
@@ -1,18 +1,18 @@
+			
+			
 			function getKey(objButton){  
 				var opr = ["+", "-", "/", "*"];
 				var res = document.getElementById('res').innerHTML;
+				var numberArray = res.match(/\d+/g);
 				if(res==0)
 					res=""
-				
 				var lastInput=res.substr(res.length - 1);
 				var lastChar = opr.find(function(element) {
 				  return element ==lastInput;
 				});
-				
 				var lastInout = opr.find(function(element) {
 				  return element ==objButton;
 				});
-				
 				if (lastChar && lastInout){
 					res = res.substring(0, res.length - 1);
 					res =res+objButton;
@@ -20,16 +20,34 @@
 				}else {
 					document.getElementById("res").innerHTML = res+objButton;	
 				}
-				
-				
-				if(isNaN(objButton)){
-					var claclRes=Math.round(calcString(res)*100)/100;
+				switch(objButton){
+					case "percent":
+						if((lastChar)){
+							var percentNum=numberArray[numberArray.length-1];
+							percentNum=(percentNum*percentNum)/100;
+							document.getElementById("res").innerHTML = res+percentNum;
+						}else{
+							var percentNum=numberArray[numberArray.length-1];
+							var pPercentNum=numberArray[numberArray.length-2];
+							if(pPercentNum){
+								var numL=(-1)*percentNum.toString().length;
+								resSlice = res.slice(0, numL);
+								percentNum=(percentNum*pPercentNum)/100;
+								document.getElementById("res").innerHTML = resSlice+""+percentNum;
+							}else{
+								document.getElementById("res").innerHTML = "0";
+							}
+						}
+					break;
+					/*case isNaN(objButton):
+						var claclRes=Math.round(calcString(res)*100)/100;
+						if (claclRes=='Infinity')
+							document.getElementById("res").innerHTML = "Cannot division by zero";
+						else 
+							document.getElementById("res").innerHTML = claclRes+objButton;
+					break;*/
 					
-					if (claclRes=='Infinity')
-						document.getElementById("res").innerHTML = "Cannot division by zero";
-					else 
-						document.getElementById("res").innerHTML = claclRes+objButton;
-				 }
+				}
 			}
 			function calc(){  
 				var res = document.getElementById('res').innerHTML;
@@ -39,12 +57,9 @@
 					document.getElementById("res").innerHTML = "Cannot division by zero";
 				else 
 					document.getElementById("res").innerHTML = claclRes;	
-					
-			
 			}
 			function clearDiv(){  
 				document.getElementById("res").innerHTML = "0";
-			
 			}
 			function calcString(fn) {
 			  return new Function('return ' + fn)();
