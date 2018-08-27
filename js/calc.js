73a4e278e46d48a568b3bdee85c40cfc4194d644
AliAkbar shahi <AliAkbar.Shahi@MahanAir.Aero>
2018-08-27 18:51:30 +0430

    add radical,power,division,negPos,backSpace functions

--- js/calc.js
+++ js/calc.js
@@ -22,12 +22,11 @@
 				}
 				switch(objButton){
 					case "percent":
+						var percentNum=numberArray[numberArray.length-1];
 						if((lastChar)){
-							var percentNum=numberArray[numberArray.length-1];
-							percentNum=(percentNum*percentNum)/100;
-							document.getElementById("res").innerHTML = res+percentNum;
+								percentNum=(percentNum*percentNum)/100;
+								document.getElementById("res").innerHTML = res+percentNum;
 						}else{
-							var percentNum=numberArray[numberArray.length-1];
 							var pPercentNum=numberArray[numberArray.length-2];
 							if(pPercentNum){
 								var numL=(-1)*percentNum.toString().length;
@@ -39,20 +38,89 @@
 							}
 						}
 					break;
-					/*case isNaN(objButton):
-						var claclRes=Math.round(calcString(res)*100)/100;
-						if (claclRes=='Infinity')
-							document.getElementById("res").innerHTML = "Cannot division by zero";
-						else 
-							document.getElementById("res").innerHTML = claclRes+objButton;
-					break;*/
-					
+					case "radical":
+						var percentNum=numberArray[numberArray.length-1];
+						if((lastChar)){
+								percentNum=Math.sqrt(percentNum);
+								document.getElementById("res").innerHTML = res+percentNum;
+						}else{
+								var numL=(-1)*percentNum.toString().length;
+								resSlice = res.slice(0, numL);
+								percentNum=Math.sqrt(percentNum);
+								document.getElementById("res").innerHTML = resSlice+""+percentNum;
+						}
+					break;
+					case "power":
+						var percentNum=numberArray[numberArray.length-1];
+						if((lastChar)){
+								percentNum=Math.pow(percentNum,2);
+								document.getElementById("res").innerHTML = res+percentNum;
+						}else{
+								var numL=(-1)*percentNum.toString().length;
+								resSlice = res.slice(0, numL);
+								percentNum=Math.pow(percentNum,2);
+								document.getElementById("res").innerHTML = resSlice+""+percentNum;
+						}
+					break;
+					case "division":
+						var percentNum=numberArray[numberArray.length-1];
+						if((lastChar)){
+								percentNum=(1/percentNum);
+								document.getElementById("res").innerHTML = res+percentNum;
+						}else{
+								var numL=(-1)*percentNum.toString().length;
+								resSlice = res.slice(0, numL);
+								percentNum=(1/percentNum);
+								document.getElementById("res").innerHTML = resSlice+""+percentNum;
+						}
+					break;
+					case "negPos":
+						var percentNum=numberArray[numberArray.length-1];
+						if((lastChar)){
+								percentNum=(-1)*percentNum;
+								document.getElementById("res").innerHTML = res+percentNum;
+						}else{
+								var numL=(-1)*percentNum.toString().length;
+								resSlice = res.slice(0, numL);
+								percentNum=(-1)*percentNum;
+								document.getElementById("res").innerHTML = resSlice+""+percentNum;
+						}
+					break;
+					case "backSpace":
+								if(isNaN(res)){
+									if((res.toString().length) != 1 ){
+										resSlice = res.slice(0, -1);
+										document.getElementById("res").innerHTML = resSlice;
+									}else{
+										document.getElementById("res").innerHTML = "0";
+									}
+								}else{
+									if((res.length) != 1 && (res.length) != 0){
+										resSlice = res.slice(0, -1);
+										document.getElementById("res").innerHTML = resSlice;
+									}else{
+										document.getElementById("res").innerHTML = "0";
+									}
+								}
+					break;
+					default:
+								if(isNaN(objButton)){
+									if(res!=0){
+										var claclRes=Math.round(calcString(res)*100)/100;
+										if (claclRes=='Infinity')
+											document.getElementById("res").innerHTML = "Cannot division by zero";
+										else 
+											document.getElementById("res").innerHTML = claclRes+objButton;
+									}else{
+										document.getElementById("res").innerHTML = "0";
+									}
+								}
 				}
+				
 			}
 			function calc(){  
 				var res = document.getElementById('res').innerHTML;
 				var claclRes=Math.round(calcString(res)*100)/100;
-				console.log(claclRes);
 				if (claclRes=='Infinity')
 					document.getElementById("res").innerHTML = "Cannot division by zero";
 				else 
